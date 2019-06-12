8d35248eaf329f701a7fab743cf3b75fa50383fe
Piyush Kumar <piyushkumar1523@gmail.com>
2019-06-12 03:15:17 -0700

    DT player seems to be doing worse. Wonder why

--- cartpole/cartpole.py
+++ cartpole/cartpole.py
@@ -1,42 +1,71 @@
 import gym
 import time
 import numpy as np
+import logging
+logging.basicConfig(level=logging.INFO)
 
-SHOW_VIS = False
-TOTAL_EPISODES = 200
-ANG_MAR = 0.02
+from players import Player, RandomPlayer, DecisionTreePlayer
+from interaction import Observation, Action, convert_event_sequence_to_training_set
 
-def play_episode(env) :
+class Constants :
+    training_episodes = 2000
+    testing_episodes = 200
+
+
+def play_episode(env, player, show_viz=False, log_events=False) :
+    logs = None
+    if log_events : logs = []
+
     obv = env.reset()
-    cart_pos, pole_angle = obv[0], obv[2]
+    state = Observation(obv)
+    if log_events : logs.append(state)
 
     total_reward = 0
 
     while True :
-        if pole_angle < -ANG_MAR : action = 0
-        elif pole_angle > 0 and pole_angle < ANG_MAR : action = 0
-        else : action = 1
-
+        action = player.play(env, state)
         obv, reward, done, info = env.step(action)
-        cart_pos, pole_angle = obv[0], obv[2]
+        state = Observation(obv)
         total_reward += reward
 
-        # env.render()
+        if log_events :
+            logs.append(Action(action))
+            logs.append(state)
 
         if done : break
 
-        # time.sleep(.1)
+        if show_viz :
+            env.render()
+            #time.sleep(.1)
 
-    return total_reward
+    return total_reward, logs
 
 env = gym.make("CartPole-v0")
 
-rewards = []
 
-for ep in range(TOTAL_EPISODES) :
-    total_reward = play_episode(env)
-    rewards.append(total_reward)
+train_logs = []
+random_player = RandomPlayer()
 
-print(f"Mean Reward: {np.mean(rewards)}")
+total_reward = 0.0
+for ep in range(Constants.training_episodes) :
+    reward, event_log = play_episode(env, random_player, log_events=True)
+    train_logs.extend(event_log)
+    total_reward += reward
+
+logging.info("Mean reward with random player: {}".format(total_reward/Constants.training_episodes))
+
+training_data, training_labels = convert_event_sequence_to_training_set(train_logs)
+logging.info("Training set size: {}".format(len(training_data)))
+
+dt_player = DecisionTreePlayer()
+dt_player.fit(training_data, training_labels)
+logging.info("Trained DT player")
+
+total_reward = 0.0
+for ep in range(Constants.testing_episodes) :
+    reward, event_log = play_episode(env, dt_player)
+    total_reward += reward
+
+logging.info("Mean reward with decision tree player: {}".format(total_reward/Constants.training_episodes))
 
 env.close()
